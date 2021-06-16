c0394abd5d34d2f81f52617dca41add9478c3219
qinyif <qinyifeng@outlook.com>
2021-06-16 15:30:35 +0800

    Add urlutils SetRawQuery

--- urlutils/url.go
+++ urlutils/url.go
@@ -1,9 +1,34 @@
 package urlutils
 
-import "strings"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
 
 func Joint(schemeHost string, path string, urlValuesEncode string) string {
 	return strings.TrimRight(schemeHost, "/") + "/" +
 		strings.TrimLeft(path, "/") + "?" +
 		strings.TrimLeft(urlValuesEncode, "?")
+}
+
+func SetRawQuery(oldUrl string, key string, value string) (string, error) {
+	u, err := url.Parse(oldUrl)
+	if err != nil {
+		return "", err
+	}
+
+	urlValues := u.Query()
+	urlValues.Set(key, value)
+
+	newUrl := ""
+	if u.User.String() == "" {
+		newUrl = fmt.Sprintf("%s://%s%s?%s#%s", u.Scheme, u.Host, u.Path, urlValues.Encode(), u.Fragment)
+
+	} else {
+		newUrl = fmt.Sprintf("%s://%s@%s%s?%s#%s", u.Scheme, u.User, u.Host, u.Path, urlValues.Encode(), u.Fragment)
+
+	}
+
+	return newUrl, nil
 }
--- urlutils/url_test.go
+++ urlutils/url_test.go
@@ -2,13 +2,10 @@ package urlutils
 
 import (
 	"fmt"
-	url4 "net/url"
 	"testing"
 )
 
 func TestJoint(t *testing.T) {
-	a := url4.Values{}
-	a.Encode()
 	url := Joint("http://m.baidu.com/", "/a/b", "?a=b")
 	fmt.Println(url)
 
@@ -20,4 +17,30 @@ func TestJoint(t *testing.T) {
 
 	url3 := Joint("http://m.baidu.com", "a/b", "c=d")
 	fmt.Println(url3)
+}
+
+func TestAppendRawQuery(t *testing.T)  {
+	u, err := SetRawQuery("postgres://user:pass@host.com:5432/path?k=v#f", "k1", "v1")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(u)
+
+	u1, err1 := SetRawQuery("https://host.com:5432/path?k=v#f", "k1", "v1")
+	if err1 != nil {
+		t.Error(err1)
+		return
+	}
+
+	fmt.Println(u1)
+
+	u1, err1 = SetRawQuery(".com:5432/path?k=v#f", "k1", "v1")
+	if err1 != nil {
+		t.Error(err1)
+		return
+	}
+
+	fmt.Println(u1)
 }
