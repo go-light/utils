b3037fd113244d4e8c97162d37cd2c7f9d4a930f
qinyif <qinyifeng@outlook.com>
2021-07-30 15:53:29 +0800

    Fix

--- sqlutils/sqlutils.go
+++ sqlutils/sqlutils.go
@@ -15,7 +15,7 @@ func Joint(query string, fieldValue []string) string {
 
 	newQuery := ""
 	for _, t := range fieldValue {
-		newQuery += strings.Replace(query, " FROM", fmt.Sprintf(",%s as `x_arg_date` FROM", t), 1) + " UNION ALL "
+		newQuery += strings.Replace(query, " FROM", fmt.Sprintf(",'%s' as `x_arg_date` FROM", t), 1) + " UNION ALL "
 	}
 
 	newQuery = strings.Trim(newQuery, " UNION ALL ") + ";"
--- sqlutils/sqlutils_test.go
+++ sqlutils/sqlutils_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestJoint(t *testing.T) {
-		fieldValues := []string{"2012", "2013", "2014"}
-		query := "Select sum(single_question_num), yearweek(stat_date) from (\nSELECT *, yearweek(stat_date) as w FROM warehouse.dws_pingce_core where stat_date >= \"2021-07-16 00:00:00\" and stat_date <= \"2021-07-22 00:00:00\") t group by w;\n;"
-		fmt.Println(Joint(query, fieldValues))
+	fieldValues := []string{"2012-12", "2013", "2014"}
+	query := "Select sum(single_question_num), yearweek(stat_date) from (\nSELECT *, yearweek(stat_date) as w FROM warehouse.dws_pingce_core where stat_date >= \"2021-07-16 00:00:00\" and stat_date <= \"2021-07-22 00:00:00\") t group by w;\n;"
+	fmt.Println(Joint(query, fieldValues))
 }
